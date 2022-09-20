d682ccffe8ed0c6ca1b8dca69d2b96be4347f0ac
Zach <zachary.hale@tuta.io>
2022-09-20 01:59:31 -0700

    Update pimp.sh

--- pimp.sh
+++ pimp.sh
@@ -30,6 +30,7 @@ sudo dnf install -y gstreamer1-libav
 sudo dnf install -y openssl
 sudo dnf install -y gnome-shell-extension-pop-shell xprop
 sudo dnf install -y nautilus-image-converter
+sudo dnf install -y webp-pixbuf-loader
 sudo dnf install -y alacarte
 sudo dnf install -y pavucontrol
 #sudo dnf install -y rpm2cpio
